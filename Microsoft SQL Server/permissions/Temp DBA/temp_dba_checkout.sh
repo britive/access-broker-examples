c0d1ad0c2e0d7cda5aa557c7720d686dc9ca06d3
palakchheda <42711310+palakchheda@users.noreply.github.com>
2025-01-17 09:35:57 -0800

    script updates after local tests

--- Microsoft SQL Server/permissions/Temp DBA/temp_dba_checkout.sh
+++ Microsoft SQL Server/permissions/Temp DBA/temp_dba_checkout.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 # Variables
-SERVER_NAME="<server_name>.database.windows.net"
-DATABASE_NAME="<database_name>"
-ADMIN_USER="<admin_username>"
-ADMIN_PASSWORD="<admin_password>"
+SERVER_NAME=$server_name
+DATABASE_NAME=$database_name
+ADMIN_USER=$admin_user
+ADMIN_PASSWORD=$admin_password
+EMAIL=$user_email
 
 # Function to generate a random 9-character password
 generate_password() {
-  tr -dc 'A-Za-z0-9@#$%^&+=_' </dev/urandom | head -c 9
+  openssl rand -base64 12 | tr -dc 'A-Za-z0-9@#$%^&+=_' | head -c 12
 }
 
 # Check if the EMAIL environment variable is set
@@ -23,15 +24,23 @@ NEW_USER=$(echo $EMAIL | awk -F'@' '{print $1}')
 # Generate a random 9-character password
 NEW_USER_PASSWORD=$(generate_password)
 
-# SQL commands to create a new user with full admin privileges
-SQL_COMMANDS="
+echo "New User Password: $NEW_USER_PASSWORD"
+
+# SQL commands to create a new login and user with full admin privileges
+SQL_COMMANDS_MASTER="
 CREATE LOGIN [$NEW_USER] WITH PASSWORD = '$NEW_USER_PASSWORD';
-CREATE USER [$NEW_USER] FOR LOGIN [$NEW_USER];
-EXEC sp_addrolemember 'db_owner', '$NEW_USER';
 "
 
-# Execute the SQL commands
-sqlcmd -S $SERVER_NAME -d $DATABASE_NAME -U $ADMIN_USER -P $ADMIN_PASSWORD -Q "$SQL_COMMANDS"
+SQL_COMMANDS_DATABASE="
+CREATE USER [$NEW_USER] FOR LOGIN [$NEW_USER] WITH DEFAULT_SCHEMA=[db_owner];
+ALTER ROLE db_owner ADD MEMBER [$NEW_USER];
+"
+
+# Execute the SQL commands in the master database to create the login
+sqlcmd -S $SERVER_NAME -d master -U $ADMIN_USER -P $ADMIN_PASSWORD -Q "$SQL_COMMANDS_MASTER"
+
+# Execute the SQL commands in the target database to create the user
+sqlcmd -S $SERVER_NAME -d $DATABASE_NAME -U $ADMIN_USER -P $ADMIN_PASSWORD -Q "$SQL_COMMANDS_DATABASE"
 
 # Output the connection string for the new user
 echo "Connection string for the new user:"
