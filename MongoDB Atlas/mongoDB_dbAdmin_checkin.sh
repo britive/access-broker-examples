bf36e2592b88a62e552a90ae8dd4091d2bd2acdb
Shahzad Ali <52764993+netJoints@users.noreply.github.com>
2025-09-02 22:32:42 -0700

    Update mongoDB_dbAdmin_checkin.sh

--- MongoDB Atlas/mongoDB_dbAdmin_checkin.sh
+++ MongoDB Atlas/mongoDB_dbAdmin_checkin.sh
@@ -2,10 +2,16 @@
 
 # Checkin routing/script will change the permissions back to DB reader only
 # MongoDB Atlas API credentials
-PUBLIC_KEY=""
-PRIVATE_KEY=""
-PROJECT_ID=""
-USERNAME=""
+
+# MongoDB Atlas API credentials
+PUBLIC_KEY="${mongoDB_public_key}"
+PRIVATE_KEY="${mongoDB_private_key}"
+PROJECT_ID="${mongoDB_project_id}"
+USERNAME="${mongoDB_username}"              # Get the full email address Britive checkout process
+
+USERNAME="${USERNAME%%@*}"                  # Extract everything before '@'
+USERNAME="${USERNAME//[^a-zA-Z0-9]/}"       # Remove any non-alphanumeric characters
+
 
 # Test connection (silent)
 TEST_RESPONSE=$(curl -s -w "%{http_code}" \
